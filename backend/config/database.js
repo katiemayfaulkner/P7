1ad99598f19c442e8b0f4a9e106900b25f15e565
katiemayfaulkner <katiemayfaulkner@users.noreply.github.com>
2021-08-23 11:08:30 +0100

    feat: connection to database

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,13 +1,40 @@
-const Sequelize = require('sequelize');
+// const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('mysql-groupomania', 'root', 'password', {
-  host: 'localhost',
-  dialect: 'mysql',
+// const sequelize = new Sequelize('localhost', 'root', 'My1$ecure2P@$$sw0rd3', {
+//   host: 'localhost',
+//   dialect: 'mysql',
 
-  pool: {
-      max: 5, //Never have more than five open connections
-      min: 0, //At a minimum, have zero open connections
-      acquire: 30000,
-      idle: 10000 //Remove a connection from the pool after the connection has not been used for 10 seconds
-  }
+//   pool: {
+//       max: 5, //Never have more than five open connections
+//       min: 0, //At a minimum, have zero open connections
+//       acquire: 30000,
+//       idle: 10000 //Remove a connection from the pool after the connection has not been used for 10 seconds
+//   }
+// });
+
+
+var mysql = require('mysql');
+  
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "My1$ecure2P@$$sw0rd3"
+});
+  
+// // Create the Database named "gfg"
+// con.connect(function (err) {
+//     if (err) throw err;
+//     console.log("Connected!");
+  
+//     con.query("CREATE DATABASE gfg",
+//         function (err, result) {
+//             if (err) throw err;
+//             console.log("Database created");
+//         });
+// });
+
+// Create the Connection
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Database connected!");
 });
--- backend/server.js
+++ backend/server.js
@@ -57,21 +57,15 @@ server.on('listening', () => {
 
 server.listen(port);
 
+//Import DATABASE config for connection, which takes place in db file
+const db = require('./config/database')
 
-//                 //Import database config
-//                 const db = require('./config/database')
+app.use(cors());
 
-//                 // TEST connection
-//                 db.authenticate()
-//                 .then(()=> console.log('Database connected!'))
-//                 .catch(err => console.log('Error: ' + err))
+app.use(bodyParser.json()); // for parsing application/json
+// app.use(bodyParser.urlencoded({ extended: true }));
 
-//                 app.use(cors());
-
-//                 app.use(bodyParser.json()); // for parsing application/json
-//                 // app.use(bodyParser.urlencoded({ extended: true }));
-
-//                 app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 
 // //Using Routes
