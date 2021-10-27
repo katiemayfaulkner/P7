04400570787355afc0eb6b01cfe5939bf0065b4e
katiemayfaulkner <katiemayfaulkner@users.noreply.github.com>
2021-10-28 00:33:22 +0200

    feat: Website recreation

---
+++ frontend/js/main.js
@@ -0,0 +1,50 @@
+/*  COMMENTS MODAL  */
+let commentsModal = document.getElementById('commentsModal');
+
+// show comments modal
+let showComments = document.getElementById('commentsBtn');
+
+showComments.onclick = function() {  
+    commentsModal.style.visibility = "visible";
+};
+
+// hide comments modal
+let hideComments = document.getElementById('closeComments');
+
+hideComments.onclick = function() {  
+    commentsModal.style.visibility = "hidden";
+}; 
+
+/*  CREATE POST MODAL  */
+let postModal = document.getElementById('postModal');
+
+// show post modal
+let showPost = document.getElementById('showPost')
+
+showPost.onclick = function() {  
+    postModal.style.visibility = "visible";
+};
+
+// hide post modal
+let hidePost = document.getElementById('closePost');
+
+hidePost.onclick = function() {  
+    postModal.style.visibility = "hidden";
+}; 
+
+/*  USER MODAL  */
+let userModal = document.getElementById('userModal');
+ 
+// show user modal
+let showUser = document.getElementById('showUser')
+
+showUser.onclick = function() {  
+    userModal.style.visibility = "visible";
+};
+
+// hide user modal
+let closeUser = document.getElementById('closeUser')
+
+closeUser.onclick = function() {  
+    userModal.style.visibility = "hidden";
+};
